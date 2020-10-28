35944980b8de80a318641c53818db3c354996dc9
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-28 19:49:02 +0530

    [Surajmal Kumar].Adding UC3.Inserting New Contacts to adrressbook.

--- Address Book System.sql
+++ Address Book System.sql
@@ -11,8 +11,17 @@ LAST_NAME varchar(250) Not Null,
 ADDRESS varchar(250) Not Null,
 CITY varchar(250) Not Null,
 STATE varchar(250) Not Null,
-ZIP int(50) unsigned not null,
-PHONE_NUMBER int(50) unsigned not null); 
+ZIP int(250) unsigned not null,
+PHONE_NUMBER varchar(250) not null); 
+
+#UseCase 3 : Inserting New Contacts to adrressbook.
+
+insert into addressbook(FIRST_NAME,LAST_NAME,ADDRESS,CITY,STATE,ZIP,PHONE_NUMBER) values ('Surajmal','Kumar','Madhyakunj','Dhanbad','Jharkhand',123456,9999988888);
+insert into addressbook(FIRST_NAME,LAST_NAME,ADDRESS,CITY,STATE,ZIP,PHONE_NUMBER) values ('ram','Kumar','ramnagar','Mirzapur','Uttar Pradesh',654321,8888899999);
+      
+                    
+
+
 
 
 
