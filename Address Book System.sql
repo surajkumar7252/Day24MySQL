9440040a93eb5f1372a1b88447c6a0a9c2cd5c0a
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-28 19:31:47 +0530

    [Surajmal Kumar].Adding UC2. Creating an addressbook Table with given attributes.

--- Address Book System.sql
+++ Address Book System.sql
@@ -2,3 +2,19 @@
 
 create database addressbook_service;
 
+#UseCase 2 : Creating an Address Book Table with given attributes.
+
+use addressbook_service;
+create table addressbook(
+FIRST_NAME  varchar(250) Not Null ,
+LAST_NAME varchar(250) Not Null,
+ADDRESS varchar(250) Not Null,
+CITY varchar(250) Not Null,
+STATE varchar(250) Not Null,
+ZIP int(50) unsigned not null,
+PHONE_NUMBER int(50) unsigned not null); 
+
+
+
+
+
