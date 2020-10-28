f60de66be4d538325d2f557d9c76158ee157867c
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-28 20:01:38 +0530

    [Surajmal Kumar].Adding UC4.Editing Existing contact using their name.

--- Address Book System.sql
+++ Address Book System.sql
@@ -12,14 +12,21 @@ ADDRESS varchar(250) Not Null,
 CITY varchar(250) Not Null,
 STATE varchar(250) Not Null,
 ZIP int(250) unsigned not null,
-PHONE_NUMBER varchar(250) not null); 
+PHONE_NUMBER varchar(250) not null,
+primary key(FIRST_NAME)); 
 
 #UseCase 3 : Inserting New Contacts to adrressbook.
 
 insert into addressbook(FIRST_NAME,LAST_NAME,ADDRESS,CITY,STATE,ZIP,PHONE_NUMBER) values ('Surajmal','Kumar','Madhyakunj','Dhanbad','Jharkhand',123456,9999988888);
 insert into addressbook(FIRST_NAME,LAST_NAME,ADDRESS,CITY,STATE,ZIP,PHONE_NUMBER) values ('ram','Kumar','ramnagar','Mirzapur','Uttar Pradesh',654321,8888899999);
-      
+ 
+#UseCase 4 : Editing Existing contact using their name. 
+
+update addressbook set FIRST_NAME='Faizal' where FIRST_NAME='Ram';
+update addressbook set CITY='Wasseupur' where FIRST_NAME='Faizal';
+
                     
+
 
 
 
